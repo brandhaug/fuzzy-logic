43bd5042d06ea9ed223bea2233628cd55962c833
brandhaug-desktop <mbrandhaug@live.no>
2018-10-31 18:19:18 +0100

    Finished

--- fuzzy_reasoning.py
+++ fuzzy_reasoning.py
@@ -47,41 +47,41 @@ def reverse_grade(position, x0, x1, clip):
 def calculate_dom(fuzzy_sets, position):
     dom = []
 
-    for i, value in enumerate(fuzzy_sets):
+    for i, x in enumerate(fuzzy_sets):
         if i == 0:
-            dom.append(reverse_grade(position, value[1][0], value[2][0], 1))
+            dom.append(reverse_grade(position, x[0], x[1], 1))
         elif i == len(distances) - 1:
-            dom.append(grade(position, value[1][0], value[2][0], 1))
+            dom.append(grade(position, x[0], x[1], 1))
         else:
-            dom.append(triangle(position, value[1][0], value[2][0], value[3][0], 1))
+            dom.append(triangle(position, x[0], x[1], x[2], 1))
     return dom
 
 
 # Distances (x)
 distances = [
-    [[0, 1], [1, 1], [2.5, 0], [10, 0]],  # A1 very_small
-    [[0, 0], [1.5, 0], [3, 1], [4.5, 0], [10, 0]],  # A2 small
-    [[0, 0], [3.5, 0], [5, 1], [6.5, 0], [10, 0]],  # A3 perfect
-    [[0, 0], [5.5, 0], [7, 1], [8.5, 0], [10, 0]],  # A4 big
-    [[0, 0], [7.5, 0], [8.5, 1], [10, 1]]  # A5
+    [1, 2.5],  # A1 very_small
+    [1.5, 3, 4.5],  # A2 small
+    [3.5, 5, 6.5],  # A3 perfect
+    [5.5, 7, 8.5],  # A4 big
+    [7.5, 8.5, 1]  # A5
 ]
 
 # Deltas (y)
 deltas = [
-    [[-5, 1], [-4, 1], [-2.5, 0], [5, 0]],  # B1 shrinking_fast
-    [[-5, 0], [-3.5, 0], [-2, 1], [-0.5, 0], [5, 0]],  # B2 shrinking
-    [[-5, 0], [-1.5, 0], [0, 1], [1.5, 0], [5, 0]],  # B3 stable
-    [[-5, 0], [0.5, 0], [2, 1], [3.5, 0], [5, 0]],  # B4 growing
-    [[-5, 0], [2.5, 0], [4, 1], [5, 1]]  # B5 growing_fast
+    [-4, -25],  # B1 shrinking_fast
+    [-3.5, -2, -0.5],  # B2 shrinking
+    [-1.5, 0, 1.5],  # B3 stable
+    [0.5, 2, 3.5],  # B4 growing
+    [2.5, 4]  # B5 growing_fast
 ]
 
 # Actions (z)
 actions = [
-    [[-10, 1], [-7, 1], [-5, 0], [10, 0]],  # C1 brake_hard
-    [[-10, 0], [-7, 0], [-4, 1], [-1, 0], [10, 0]],  # C2 slow_down
-    [[-10, 0], [-3, 0], [0, 1], [3, 0], [10, 0]],  # C3 none
-    [[-10, 0], [1, 0], [4, 1], [7, 0], [10, 0]],  # C4 speed_up
-    [[-10, 0], [5, 0], [8, 1], [10, 1]]  # C5 floor_it
+    [-7, -5],  # C1 brake_hard
+    [-7, -4, -1],  # C2 slow_down
+    [-3, 0, 3],  # C3 none
+    [1, 4, 7],  # C4 speed_up
+    [5, 8]  # C5 floor_it
 ]
 
 # Inputs
@@ -93,6 +93,9 @@ Part 1: Fuzzification - Degree of membership (DOM)
 """
 distance_dom = calculate_dom(distances, distance_input)
 delta_dom = calculate_dom(deltas, delta_input)
+
+print('Distance DOM', distance_dom)
+print('Delta DOM', delta_dom)
 
 """
 Part 2: Rule evaluation
@@ -116,31 +119,25 @@ Part 3: Rule aggregation
 """
 min_x = -10
 max_x = 10
-
-actions_detailed = [
-    [[-10, 1], [-9, 1.0], [-8, 1.0], [-7, 1], [-6, 0.5], [-5, 0], [-4, 0], [-3, 0], [-2, 0], [-1, 0], [0, 0], [1, 0],
-     [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]],
-    [[-10, 0], [-9, 0], [-8, 0], [-7, 0], [-6, 0.33], [-5, 0.67], [-4, 1], [-3, 0.67], [-2, 0.33], [-1, 0], [0, 0],
-     [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]],
-    [[-10, 0], [-9, 0], [-8, 0], [-7, 0], [-6, 0], [-5, 0], [-4, 0], [-3, 0], [-2, 0.33], [-1, 0.67], [0, 1], [1, 0.67],
-     [2, 0.33], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]],
-    [[-10, 0], [-9, 0], [-8, 0], [-7, 0], [-6, 0], [-5, 0], [-4, 0], [-3, 0], [-2, 0], [-1, 0], [0, 0], [1, 0],
-     [2, 0.33], [3, 0.67], [4, 1], [5, 0.67], [6, 0.33], [7, 0], [8, 0], [9, 0], [10, 0]],
-    [[-10, 0], [-9, 0], [-8, 0], [-7, 0], [-6, 0], [-5, 0], [-4, 0], [-3, 0], [-2, 0], [-1, 0], [0, 0], [1, 0],
-     [2, 0], [3, 0], [4, 0], [5, 0], [6, 0.5], [7, 1], [8, 1], [9, 1], [10, 1]],
-]
+step = 1
 
 highest_arr = [0] * 21
 
-for action_index, actions in enumerate(actions_detailed):
-    for i, coordinates in enumerate(actions):
-        if coordinates[1] > action_dom[action_index]:
-            current_height = action_dom[action_index]
-        else:
-            current_height = coordinates[1]
+for position in range(min_x, max_x + 1):
+    for action_index, x in enumerate(actions):
+        val = 0
 
-        if current_height > highest_arr[i]:
-            highest_arr[i] = current_height
+        if action_index == 0:
+            val = reverse_grade(position, x[0], x[1], action_dom[action_index])
+        elif action_index == len(distances) - 1:
+            val = grade(position, x[0], x[1], action_dom[action_index])
+        else:
+            val = triangle(position, x[0], x[1], x[2], action_dom[action_index])
+
+        if val > highest_arr[position + 10]:
+            highest_arr[position + 10] = val
+
+print(highest_arr)
 
 dividend = 0.0
 divisor = 0.0
@@ -159,7 +156,24 @@ for val in highest_arr:
         values_count[index] += 1
     divisor += val
 
-defuzzified_output = dividend / float(divisor)
-print defuzzified_output
+defuzzified_output = dividend / divisor
+print('Defuzzified output', defuzzified_output)
 
+highest_val = 0
+highest_index = 0
 
+for action_index, x in enumerate(actions):
+    temp_val = 0
+
+    if action_index == 0:
+        temp_val = reverse_grade(defuzzified_output, x[0], x[1], 1)
+    elif action_index == len(distances) - 1:
+        temp_val = grade(defuzzified_output, x[0], x[1], 1)
+    else:
+        temp_val = triangle(defuzzified_output, x[0], x[1], x[2], 1)
+
+    if temp_val > highest_val:
+        highest_val = temp_val
+        highest_index = action_index
+
+print('Action: C%d' % (highest_index + 1))
